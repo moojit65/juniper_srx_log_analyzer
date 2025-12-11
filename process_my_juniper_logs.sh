2667301321ba844838910d9dc18375e427d406e9
moojit <moojit@moojit.net>
2025-12-10 19:21:40 -0700

    tweak to help bash script

--- process_my_juniper_logs.sh
+++ process_my_juniper_logs.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 if [[ "$1" == "-h" || "$1" == "--help" || "$1" == "help" ]]; then
-    cat << EOF
+cat << EOF
 Usage: process_my_juniper_logs.sh %1 %2 %3
 
-A short, useful script that does something cool.
+Helper script for processing srx log files.
 
 Options:
-  -h, --help      Show this help
-  %1              srx hostname
-  %2              fullpath and name of file(s) to be parsed
-  %3              if present, any character will enable multi-threading across all cpu cores
-                  for performance gain
+-h, --help      Show this help
+%1              srx hostname
+%2              fullpath and name of file(s) to be parsed
+%3              if present, any character will enable multi-threading across all cpu cores
+				for performance gain
 
-Example:
-  ./process_my_juniper_logs.sh john_doe /var/log/messages enable
-  ./process_my_juniper_logs.sh john_doe /var/log/messages* enable
-  ./process_my_juniper_logs.sh john_doe /var/log/messages disable
+Examples:
+./process_my_juniper_logs.sh john_doe /var/log/messages enable
+./process_my_juniper_logs.sh john_doe /var/log/messages* enable
+./process_my_juniper_logs.sh john_doe /var/log/messages disable
 EOF
     exit 0
 fi
@@ -58,7 +58,7 @@ for file in "$MYSCRIPT_DIR"/*; do
     if $MYMULTI == 1; then
         echo "python3 srx_report.py -p $file -v &"
         python3 srx_report.py -p $file &
-    elif
+    else
         echo "python3 srx_report.py -p $file -v"
         python3 srx_report.py -p $file
     fi
